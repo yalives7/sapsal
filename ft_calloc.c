14dea633dc63aa22ba5e96f81ddd4e6fb5c21750
yalives7 <ungorsevilay03@gmail.com>
2024-11-03 18:20:16 +0300

    Add files via upload

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+void ft_bzero(void *s, size_t n);
+void *ft_calloc(size_t count,size_t size)
+{
+    size_t result = count * size;
+    unsigned char *a ;
+     a = (unsigned char *)malloc(result);
+
+    if(a == NULL)
+        return NULL;
+    ft_bzero(a,result);
+    return a;
+}
+int main()
+{
+    char *a = ft_calloc(3,20);
+    if(a == NULL)
+    printf("olmadi");
+    printf("%s %p",a,a);
+    char *b;
+    printf("\n%p",b);
+    printf("\n%d",(int)strlen(a));
+}
